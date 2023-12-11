33fbb905648a9fd0b4b1f704ad62a1a1dc72bfca
tldav <tldavis09@yahoo.com>
2023-12-11 13:17:00 -0600

    Attempt No. 642

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import LandingView from "@/view/landing/LandingView";
-
-function Home() {
-	return <LandingView />;
-}
-
-export default Home;
--- src/region/search/searchBar/SearchBarRegion.tsx
+++
@@ -1,7 +0,0 @@
-import { TextField as MUITextField } from "@mui/material";
-
-function SearchBarRegion() {
-	return <MUITextField />;
-}
-
-export default SearchBarRegion;
