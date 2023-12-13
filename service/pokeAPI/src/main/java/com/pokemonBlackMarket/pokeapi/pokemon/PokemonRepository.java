2aa2c585c3fed193a40e267b8db53a7e6fcf9df3
tldav <tldavis09@yahoo.com>
2023-12-13 12:30:45 -0600

    Switch to node for pokemon service

--- service/pokeAPI/src/main/java/com/pokemonBlackMarket/pokeapi/pokemon/PokemonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.pokemonBlackMarket.pokeapi.pokemon;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface PokemonRepository extends ListCrudRepository<Pokemon, Integer> {
-}
--- service/pokeAPI/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS pokemon (
-   id INT AUTO_INCREMENT,
-   name varchar(250) NOT NULL,
-   PRIMARY KEY (id)
-);
