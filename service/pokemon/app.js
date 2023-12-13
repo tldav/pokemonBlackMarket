a3c2ed3da634ceda220724a75b1572c13dcd6524
tldav <tldavis09@yahoo.com>
2023-12-13 13:10:26 -0600

    add graphql dependency and implement simple query

--- service/pokemon/app.js
+++ service/pokemon/app.js
@@ -2,10 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Bueno, Tio Humberto");
-});
-
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
+});
+
+const { request, gql } = require("graphql-request");
+const pokeUrl = "https://beta.pokeapi.co/graphql/v1beta";
+
+const query = gql`
+	{
+		pokemon_v2_pokemon(limit: 1, where: { id: { _eq: 1 } }) {
+			name
+			pokemon_v2_pokemontypes {
+				pokemon_v2_type {
+					name
+				}
+			}
+		}
+	}
+`;
+
+app.get("/", (req, res) => {
+	request(pokeUrl, query).then((data) => res.json(data));
 });
